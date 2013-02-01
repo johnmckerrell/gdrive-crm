2b908e2d9cc2c7067ec14e4482642e408b6fce4b
John McKerrell <john@mckerrell.net>
2013-02-01 12:33:37 +0000

    Adding attribution

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,6 +1,7 @@
 class FeedbackMailer < ActionMailer::Base
   default from: "#{GDRIVE_CRM_EMAIL_FROM_NAME} <#{GDRIVE_CRM_EMAIL_FROM}>"
 
+  # Borrowed from Gollum
   def self.cname(name, char_white_sub = '-', char_other_sub = '-')
     name.respond_to?(:gsub) ?
       name.gsub(%r{\s},char_white_sub).gsub(%r{[<>+]}, char_other_sub).downcase :
