217018fec700c5350b4bed1352f4497a99e04c7a
John McKerrell <john@mckerrell.net>
2013-06-03 14:19:48 +0100

    Feedback reference cannot be null on email attempts

--- db/migrate/20130529160642_create_email_attempts.rb
+++ db/migrate/20130529160642_create_email_attempts.rb
@@ -1,7 +1,7 @@
 class CreateEmailAttempts < ActiveRecord::Migration
   def change
     create_table :email_attempts do |t|
-      t.references :feedback
+      t.references :feedback, :null => false
       t.string :status
       t.string :failure_status
       t.string :email_address
