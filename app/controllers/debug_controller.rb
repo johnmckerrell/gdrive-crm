2213b93bb738f1e803c99b4cf14ad4245472f6fd
John McKerrell <john@mckerrell.net>
2015-10-01 21:51:10 +0100

    Adding a test method which requires a username.

--- app/controllers/debug_controller.rb
+++ app/controllers/debug_controller.rb
@@ -2,6 +2,27 @@ class DebugController < ApplicationController
 
   skip_before_filter :authenticate_user!
 
+  def get_auth_details
+    begin
+      @user, @pass = ActionController::HttpAuthentication::Basic::user_name_and_password(request)
+    rescue NoMethodError => e
+      @user = nil
+      @pass = nil
+    end
+
+  end
+
+  def echo_auth
+    get_auth_details
+
+    if @user.nil?
+      render :status => 401, :message => "Unauthorized", :nothing => true
+    else
+      echo
+    end
+
+  end
+
   def echo
     json = {}
     headers = {}
@@ -11,17 +32,10 @@ class DebugController < ApplicationController
       end
     end
 
-    begin
-      user, pass = ActionController::HttpAuthentication::Basic::user_name_and_password(request)
-    rescue NoMethodError => e
-      user = nil
-      pass = nil
-    end
-
     json[:headers] = headers
     json[:raw_post] = request.raw_post
     json[:params] = request.params
-    json[:auth] = { user: user, pass: pass }
+    json[:auth] = { user: @user, pass: @pass }
 
     render :json => json
   end
